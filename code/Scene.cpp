47978d8076d1446324d1f41091de9442de77a7f8
Marko Kostic <archmarko92@gmail.com>
2023-12-18 11:22:03 +0100

    Move small ball 2 meters above the big ball

--- code/Scene.cpp
+++ code/Scene.cpp
@@ -47,7 +47,7 @@ Scene::Initialize
 */
 void Scene::Initialize() {
 	Body body;
-	body.m_position = Vec3( 0, 0, 0 );
+	body.m_position = Vec3( 0, 0, 2 );
 	body.m_orientation = Quat( 0, 0, 0, 1 );
 	body.m_shape = new ShapeSphere( 1.0f );
 	m_bodies.push_back( body );
