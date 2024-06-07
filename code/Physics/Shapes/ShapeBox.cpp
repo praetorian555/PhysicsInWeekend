5b5ea2c35637ac952931b79d3827981ca1ff2281
Marko Kostic <archmarko92@gmail.com>
2024-06-07 21:21:05 +0200

    Implement Build() and InetriaTensor() functions for the box shape

--- code/Physics/Shapes/ShapeBox.cpp
+++ code/Physics/Shapes/ShapeBox.cpp
@@ -17,7 +17,23 @@ ShapeBox::Build
 ====================================================
 */
 void ShapeBox::Build( const Vec3 * pts, const int num ) {
-	// TODO: Add code
+	for (int i = 0; i < num; i++)
+	{
+		m_bounds.Expand(pts[i]);
+	}
+
+	m_points.clear();
+	m_points.emplace_back(m_bounds.mins.x, m_bounds.mins.y, m_bounds.mins.z);
+	m_points.emplace_back(m_bounds.maxs.x, m_bounds.mins.y, m_bounds.mins.z);
+	m_points.emplace_back(m_bounds.mins.x, m_bounds.maxs.y, m_bounds.mins.z);
+	m_points.emplace_back(m_bounds.mins.x, m_bounds.mins.y, m_bounds.maxs.z);
+
+	m_points.emplace_back(m_bounds.maxs.x, m_bounds.maxs.y, m_bounds.maxs.z);
+	m_points.emplace_back(m_bounds.mins.x, m_bounds.maxs.y, m_bounds.maxs.z);
+	m_points.emplace_back(m_bounds.maxs.x, m_bounds.mins.y, m_bounds.maxs.z);
+	m_points.emplace_back(m_bounds.maxs.x, m_bounds.maxs.y, m_bounds.mins.z);
+
+	m_centerOfMass = (m_bounds.mins + m_bounds.maxs) * 0.5f;
 }
 
 /*
@@ -39,10 +55,25 @@ ShapeBox::InertiaTensor
 ====================================================
 */
 Mat3 ShapeBox::InertiaTensor() const {
-	Mat3 tensor;
-	
-	// TODO: Add code
+	const float dx = m_bounds.maxs.x - m_bounds.mins.x;
+	const float dy = m_bounds.maxs.y - m_bounds.mins.y;
+	const float dz = m_bounds.maxs.z - m_bounds.mins.z;
 
+	Mat3 tensor;
+	tensor.rows[0][0] = (dy * dy + dz * dz) / 12.0f;
+	tensor.rows[1][1] = (dx * dx + dz * dz) / 12.0f;
+	tensor.rows[2][2] = (dx * dx + dy * dy) / 12.0f;
+
+	const Vec3 center_of_mass = (m_bounds.mins + m_bounds.maxs) * 0.5f;
+	const Vec3 r = Vec3(0, 0, 0) - center_of_mass;
+	const float r2 = r.GetLengthSqr();
+
+	Mat3 pat_tensor;
+	pat_tensor.rows[0] = Vec3(r2 - r.x * r.x, r.x * r.y, r.x * r.z);
+	pat_tensor.rows[1] = Vec3(r.y * r.x, r2 - r.y * r.y, r.y * r.z);
+	pat_tensor.rows[2] = Vec3(r.z * r.x, r.z * r.y, r2 - r.z * r.z);
+
+	tensor += pat_tensor;
 	return tensor;
 }
 
