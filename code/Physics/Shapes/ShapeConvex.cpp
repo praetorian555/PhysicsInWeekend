f2b22a263ed0e7ecf58c888b687c789c00b2290e
Marko Kostic <archmarko92@gmail.com>
2024-06-09 21:40:06 +0200

    Finish all functions for building a convex hull

--- code/Physics/Shapes/ShapeConvex.cpp
+++ code/Physics/Shapes/ShapeConvex.cpp
@@ -184,8 +184,179 @@ void RemoveInternalPoints(const std::vector<Vec3>& hull_points, const std::vecto
 	}
 }
 
-void BuildConvexHull( const std::vector< Vec3 > & verts, std::vector< Vec3 > & hullPts, std::vector< tri_t > & hullTris ) {
-	// TODO: Add code
+bool IsEdgeUnique(const std::vector<tri_t>& triangles, const std::vector<int>& facing_triangles, int triangle_index_to_ignore, const edge_t& edge)
+{
+	for (int i = 0; i < facing_triangles.size(); i++)
+	{
+		const int triangle_index = facing_triangles[i];
+		if (triangle_index == triangle_index_to_ignore)
+		{
+			continue;
+		}
+
+		const tri_t& triangle = triangles[triangle_index];
+		edge_t edges[3];
+		edges[0].a = triangle.a;
+		edges[0].b = triangle.b;
+
+		edges[1].a = triangle.b;
+		edges[1].b = triangle.c;
+
+		edges[2].a = triangle.c;
+		edges[2].b = triangle.a;
+
+		for (int e = 0; e < 3; e++)
+		{
+			if (edge == edges[e])
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+void AddPoint(std::vector<Vec3>& hull_points, std::vector<tri_t>& hull_triangles, const Vec3& point_to_add)
+{
+	std::vector<int> facing_triangles;
+	for (int i = static_cast<int>(hull_triangles.size()) - 1; i >= 0; i--)
+	{
+		const tri_t& triangle = hull_triangles[i];
+		const Vec3& a = hull_points[triangle.a];
+		const Vec3& b = hull_points[triangle.b];
+		const Vec3& c = hull_points[triangle.c];
+
+		const float dist = DistanceFromTriangle(a, b, c, point_to_add);
+		if (dist > 0.0f)
+		{
+			facing_triangles.push_back(i);
+		}
+	}
+
+	// Now find all edges that are unique in the facing triangles, these will be the edges that form the new triangles
+	std::vector<edge_t> unique_edges;
+	for (int i = 0; i < facing_triangles.size(); i++)
+	{
+		const int triangle_index = facing_triangles[i];
+		const tri_t& triangle = hull_triangles[triangle_index];
+
+		edge_t edges[3];
+		edges[0].a = triangle.a;
+		edges[0].b = triangle.b;
+
+		edges[1].a = triangle.b;
+		edges[1].b = triangle.c;
+
+		edges[2].a = triangle.c;
+		edges[2].b = triangle.a;
+
+		for (int e = 0; e < 3; e++)
+		{
+			const int triangle_index_to_ignore = triangle_index;
+			if (IsEdgeUnique(hull_triangles, facing_triangles, triangle_index_to_ignore, edges[e]))
+			{
+				unique_edges.push_back(edges[e]);
+			}
+		}
+	}
+
+	// Remove all the facing triangles
+	for (int i = 0; i < facing_triangles.size(); i++)
+	{
+		hull_triangles.erase(hull_triangles.begin() + facing_triangles[i]);
+	}
+
+	// Add new point
+	hull_points.push_back(point_to_add);
+	const int new_point_index = hull_points.size() - 1;
+
+	// Add new triangles for each unique edge
+	for (int i = 0; i < unique_edges.size(); i++)
+	{
+		const edge_t& edge = unique_edges[i];
+		tri_t new_triangle;
+		new_triangle.a = edge.a;
+		new_triangle.b = edge.b;
+		new_triangle.c = new_point_index;
+		hull_triangles.push_back(new_triangle);
+	}
+}
+
+void RemoveUnreferencedVertices(std::vector<Vec3>& hull_points, std::vector<tri_t>& hull_triangles)
+{
+	for (int point_index = 0; point_index < hull_points.size(); point_index++)
+	{
+		bool is_used = false;
+		for (int triangle_index = 0; triangle_index < hull_triangles.size(); triangle_index++)
+		{
+			const tri_t& triangle = hull_triangles[triangle_index];
+			if (triangle.a == point_index || triangle.b == point_index || triangle.c == point_index)
+			{
+				is_used = true;
+				break;
+			}
+		}
+
+		if (is_used)
+		{
+			continue;
+		}
+
+		// Correct point indices in all of the triangles. If point index was larger then the index we are removing, reduce it by one
+		for (int triangle_index = 0; triangle_index < hull_triangles.size(); triangle_index++)
+		{
+			tri_t& triangle = hull_triangles[triangle_index];
+			if (triangle.a > point_index)
+			{
+				triangle.a--;
+			}
+			if (triangle.b > point_index)
+			{
+				triangle.b--;
+			}
+			if (triangle.c > point_index)
+			{
+				triangle.c--;
+			}
+
+			hull_points.erase(hull_points.begin() + point_index);
+			point_index--;
+		}
+	}
+}
+
+void ExpandConvexHull(std::vector<Vec3>& hull_points, std::vector<tri_t>& hull_triangles, const std::vector<Vec3>& vertices)
+{
+	std::vector<Vec3> external_vertices = vertices;
+
+	// Remove all vertices in the external_vertices that are inside the current convex hull
+	RemoveInternalPoints(hull_points, hull_triangles, external_vertices);
+
+	while (external_vertices.size() > 0)
+	{
+		// Select any point still in external_vertices, and pick a point that is furthest in that direction
+		const int furthest_point_index = FindPointFurthestInDir(external_vertices.data(), external_vertices.size(), external_vertices[0]);
+		const Vec3 furthest_point = external_vertices[furthest_point_index];
+
+		// Remove the furthest point from the external vertices and add it to the hull
+		external_vertices.erase(external_vertices.begin() + furthest_point_index);
+		AddPoint(hull_points, hull_triangles, furthest_point);
+
+		// Check now if there are any new internal points and remove them from consideration
+		RemoveInternalPoints(hull_points, hull_triangles, external_vertices);
+	}
+
+	// Remove all points that are not referenced by any of the hull triangles
+	RemoveUnreferencedVertices(hull_points, hull_triangles);
+}
+
+void BuildConvexHull(const std::vector< Vec3 >& verts, std::vector< Vec3 >& hullPts, std::vector< tri_t >& hullTris) {
+	if (verts.size() < 4)
+	{
+		return;
+	}
+	BuildTetrahedron(verts.data(), verts.size(), hullPts, hullTris);
+	ExpandConvexHull(hullPts, hullTris, verts);
 }
 
 /*
@@ -193,7 +364,7 @@ void BuildConvexHull( const std::vector< Vec3 > & verts, std::vector< Vec3 > & h
 ShapeConvex::Build
 ====================================================
 */
-void ShapeConvex::Build( const Vec3 * pts, const int num ) {
+void ShapeConvex::Build(const Vec3* pts, const int num) {
 	// TODO: Add code
 }
 
@@ -202,9 +373,9 @@ void ShapeConvex::Build( const Vec3 * pts, const int num ) {
 ShapeConvex::Support
 ====================================================
 */
-Vec3 ShapeConvex::Support( const Vec3 & dir, const Vec3 & pos, const Quat & orient, const float bias ) const {
+Vec3 ShapeConvex::Support(const Vec3& dir, const Vec3& pos, const Quat& orient, const float bias) const {
 	Vec3 supportPt;
-	
+
 	// TODO: Add code
 
 	return supportPt;
@@ -215,7 +386,7 @@ Vec3 ShapeConvex::Support( const Vec3 & dir, const Vec3 & pos, const Quat & orie
 ShapeConvex::GetBounds
 ====================================================
 */
-Bounds ShapeConvex::GetBounds( const Vec3 & pos, const Quat & orient ) const {
+Bounds ShapeConvex::GetBounds(const Vec3& pos, const Quat& orient) const {
 	Vec3 corners[8];
 	corners[0] = Vec3(m_bounds.mins.x, m_bounds.mins.y, m_bounds.mins.z);
 	corners[1] = Vec3(m_bounds.maxs.x, m_bounds.mins.y, m_bounds.mins.z);
@@ -240,7 +411,7 @@ Bounds ShapeConvex::GetBounds( const Vec3 & pos, const Quat & orient ) const {
 ShapeConvex::FastestLinearSpeed
 ====================================================
 */
-float ShapeConvex::FastestLinearSpeed( const Vec3 & angularVelocity, const Vec3 & dir ) const {
+float ShapeConvex::FastestLinearSpeed(const Vec3& angularVelocity, const Vec3& dir) const {
 	float max_speed = 0.0f;
 	for (int i = 0; i < m_points.size(); i++)
 	{
